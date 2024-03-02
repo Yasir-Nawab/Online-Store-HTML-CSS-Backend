7e8ac5af5d6e76416564b86316b168b783a85882
Yasir Nawab <115151765+Yasir-Nawab@users.noreply.github.com>
2024-03-02 18:15:10 +0500

    Update connection.js

--- server/models/connection.js
+++ server/models/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require ("mongoose");
-mongoose.connect("mongodb://localhost:27017/propertysale")
+mongoose.connect("mongodb://0.0.0.0:27017/propertysale")
 
 require("./propertyModel");
 require("./findModel");
